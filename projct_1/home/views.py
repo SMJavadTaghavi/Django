a719cd2b0e44b9f7cebd8fa22c9efd7c6e6dd725
Javad Taghavi <148636438+SMJavadTaghavi@users.noreply.github.com>
2025-05-03 14:11:28 +0330

    Add files via upload

---
+++ projct_1/home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+'''
+def say_hello(request):
+    return HttpResponse('Hello User...!')
+'''
+def home(request):
+    return render(request, 'home.html')
+
+def say_hello(request):
+    person = {'name':'admin'}
+    return render(request, 'hello.html', context=person)
